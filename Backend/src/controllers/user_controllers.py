a95373023c7c2085d61c178cc15c34427e09de0f
Pablo Sanabria Becerra <pablosanabria.sys@gmail.com>
2024-09-16 23:40:59 -0400

    Init Backend

---
+++ Backend/src/controllers/user_controllers.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException
+from src.schemas.user import UserCreate, User
+from src.services.user_service import UserService
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.post("/", response_model=User)
+def create_user(user: UserCreate):
+    return UserService.create_user(user)
+
+@router.get("/{user_id}", response_model=User)
+def read_user(user_id: int):
+    user = UserService.get_user(user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return user
