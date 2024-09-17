d4bfcb6eeb121a5c798d2521b51b98ad703c805c
cosmito123 <cosmitotuya@gmail.com>
2024-09-16 23:42:09 -0400

    Mejora del logo sigerem

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -18,7 +18,7 @@ export default function NavBar() {
         <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
           <div className="flex flex-row items-center justify-between p-4">
             <Link
-              to="/"
+              to="/home"
               className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark:text-white focus:outline-none focus:shadow-outline"
             >
               SIGEREM
