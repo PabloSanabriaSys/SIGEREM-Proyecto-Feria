b231a102405d774bad548cde7df08fd5cbbf3507
Leonardo <releegva@gmail.com>
2024-09-19 14:24:02 -0400

    feat(frontend): implemented websocket connection to backend.

--- Frontend/src/Pages/Nosotros.jsx
+++ Frontend/src/Pages/Nosotros.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Nosotros = () => {
+  const WS_URL = "ws://127.0.0.1:8000/ws/2";
+  const connection = useRef(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const socket = new WebSocket(WS_URL);
+    connection.current = socket;
+
+    socket.addEventListener("open", (e) => {
+      console.log("connection established");
+      socket.send("connection established");
+    });
+
+    socket.addEventListener("message", (e) => {
+      console.log("message recieved from server ", e.data);
+      setMessage(e.data);
+    });
+
+    socket.addEventListener("close", () => {
+      console.log("conexion cerrada");
+    });
+
+    socket.addEventListener("error", (error) => {
+      console.error("websocket error: ", error);
+    });
+
+    return () => {
+      if (connection.current) {
+        connection.current.close();
+      }
+    };
+  }, [WS_URL]);
+
   return (
     <div className="bg-background">
       <h1>Nosotros</h1>
-      <h2>Pinpon</h2>
+      <span>Message: </span>
+      <span>{message}</span>
     </div>
   );
 };
