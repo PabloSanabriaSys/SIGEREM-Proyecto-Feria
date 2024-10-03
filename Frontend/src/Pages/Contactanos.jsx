05d982d954b6199c07350b2602d904a151deba5c
cosmito123 <cosmitotuya@gmail.com>
2024-10-03 00:30:30 -0400

    Vista home sin libreria Frame Motion

--- Frontend/src/Pages/Contactanos.jsx
+++ Frontend/src/Pages/Contactanos.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import NeuralNetworkAnimation from '../components/general/NeuralNetworkAnimation';
-import { neuronas } from '../components/constants/Neuronas';
-import { animationSiregem } from "../components/constants/AnimationSiregem";
-import ContactForm from '../components/ContactForm';
 
 const Contactanos = () => {
   return (
     <div className="p-0 w-full bg-background">
       <div className='bg-black h-screen flex items-center justify-center'>
-        <NeuralNetworkAnimation circles={animationSiregem}/>
         <div className='bg-gray-500 rounded'>
           <h1 className='p-4 font-mono font-bold'>
             TIENES DUDAS, CONTACTANOS
