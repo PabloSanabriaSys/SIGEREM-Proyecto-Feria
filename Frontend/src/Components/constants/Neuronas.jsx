8c09340332c836aa14177ac89ac2769f319db07f
cosmito123 <cosmitotuya@gmail.com>
2024-10-02 23:08:21 -0400

    Ultimos camios en el front

---
+++ Frontend/src/Components/constants/Neuronas.jsx
@@ -0,0 +1,18 @@
+// Coordenadas para formar la palabra "SIREGEM" usando círculos
+
+export const neuronas = [
+    
+  { cx: -200, cy: 500, r: 10 },
+  { cx: -100, cy: 100, r: 10 },
+  { cx: 0, cy: 500, r: 10 },
+  { cx: 100, cy: 100, r: 10 },
+  { cx: 200, cy: 500, r: 10 },
+  { cx: 300, cy: 100, r: 10 },
+  { cx: 400, cy: 500, r: 10 },
+  { cx: 500, cy: 100, r: 10 },
+  { cx: 600, cy: 500, r: 10 },
+  { cx: 700, cy: 100, r: 10 },
+  { cx: 800, cy: 500, r: 10 },
+  { cx: 900, cy: 100, r: 10 },
+  { cx: 1000, cy: 500, r: 10 },
+  ];
