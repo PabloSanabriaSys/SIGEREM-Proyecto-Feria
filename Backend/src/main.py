02601154a2ad3cab35aad13c88dda7a87270726d
Pablo Sanabria Becerra <pablosanabria.sys@gmail.com>
2024-10-04 05:50:33 -0400

    llajuita

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -4,9 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+allowed_origins = [
+    "http://localhost:5173",  # Asumiendo que este es el puerto de su aplicación React
+    "http://localhost:3000",  # Otro puerto común para desarrollos en React
+    "https://localhost:5173", # Versión HTTPS
+    "https://localhost:3000", # Versión HTTPS
+    "https://192.168.1.4:5173",  # Reemplace con su IP específica
+    "https://192.168.1.4:5173", # Versión HTTPS de su IP
+    "https://192.168.1.3:5173", 
+    "https://192.168.1.3:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,4 +35,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000,asl_keyfile="./localhost+2-key.pem",  # Ruta al archivo de clave privada generado por mkcert
+        ssl_certfile="./localhost+2.pem")
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,19 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
     },
   },
 })
